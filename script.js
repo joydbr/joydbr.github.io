dd0924a60328a526e7f6872966488b9a64795160
Joydbr <joilsonskr@hotmail.com>
2021-01-16 15:37:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -593,7 +593,7 @@ eegdbap15.map((item, index)=>{
     eegtdes.querySelector('.gat15req1').innerHTML = item.req[0];
     eegtdes.querySelector('.gat15req2').innerHTML = item.req[1];
     eegtdes.querySelector('.gat15req3').innerHTML = item.req[2];
-    if (rssreq != undefined) {
+    if (rssreq != 'undefined') {
         eegtdes.querySelector('.mtotalap15').innerHTML = `${item.rssreq[0]} ${item.rssreq[1]} ${item.rssreq[3]} ${item.rssreq[4]} ${item.rssreq[5]} ${item.rssreq[6]}`
     }
     
