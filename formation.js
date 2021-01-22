78582f05a12767cdfb02427524f711a5580e643c
Joydbr <joilsonskr@hotmail.com>
2021-01-21 21:33:18 -0300

    Update formation.js

--- formation.js
+++ formation.js
@@ -14,43 +14,43 @@ let form = [
 ];
 
 let firt = [
-    {id: 'GEI', title: 'General Enhancement II', desc:'Increase all fighters HP by 800 & ATK by 100', costs:'670000 Gold & 36 Points'},
+    {id: 'GEI', title: 'General Enhancement II<hr>', desc:'Increase all fighters HP by 800 & ATK by 100<hr>', costs:'670000 Gold & 36 Points'},
 
-    {id: 'DF', title: 'Strength Improvement', desc:'Increase DPS fighters ATK by 250', costs:'670000 Gold & 36 Points'},
-    {id: 'DFM', title: 'Restriction of Defense Offense', desc:'Reduces DPS fighters damage taken from GANK by 10%', costs:'670000 Gold & 46 Points'},
-    {id: 'DFU', title: 'Restriction of Offense Offense', desc:'Increase DPS fighters damage dealt against GANK by 10%', costs:'670000 Gold & 56 Points'},
+    {id: 'DF', title: 'Strength Improvement<hr>', desc:'Increase DPS fighters ATK by 250<hr>', costs:'670000 Gold & 36 Points'},
+    {id: 'DFM', title: 'Restriction of Defense Offense<hr>', desc:'Reduces DPS fighters damage taken from GANK by 10%<hr>', costs:'670000 Gold & 46 Points'},
+    {id: 'DFU', title: 'Restriction of Offense Offense<hr>', desc:'Increase DPS fighters damage dealt against GANK by 10%<hr>', costs:'670000 Gold & 56 Points'},
 
-    {id: 'OF', title: 'Technique Improvement', desc:'Increase GANK fighters HP by 1000 & ATK by 150', costs:'670000 Gold & 36 Points'},
-    {id: 'OFM', title: 'Restriction of Defense Skill', desc:'Reduces GANK fighters damage taken from TANK by 10%', costs:'670000 Gold & 46 Points'},
-    {id: 'OFU', title: 'Restriction of Offense Skill', desc:'Increase GANK fighters damage dealt against TANK by 10%', costs:'670000 Gold & 56 Points'},
+    {id: 'OF', title: 'Technique Improvement<hr>', desc:'Increase GANK fighters HP by 1000 & ATK by 150<hr>', costs:'670000 Gold & 36 Points'},
+    {id: 'OFM', title: 'Restriction of Defense Skill<hr>', desc:'Reduces GANK fighters damage taken from TANK by 10%<hr>', costs:'670000 Gold & 46 Points'},
+    {id: 'OFU', title: 'Restriction of Offense Skill<hr>', desc:'Increase GANK fighters damage dealt against TANK by 10%<hr>', costs:'670000 Gold & 56 Points'},
 
-    {id: 'TI', title: 'Health Mastery', desc:'Increase TANK fighters HP by 2000', costs:'670000 Gold & 36 Points'},
-    {id: 'TIM', title: 'Restriction of Defense Offense', desc:'Reduces TANK fighters damage taken from DPS by 10%', costs:'670000 Gold & 46 Points'},
-    {id: 'TIU', title: 'Restriction of Offense Offense', desc:'Increase TANK fighters damage dealt against DPS by 10%', costs:'670000 Gold & 56 Points'},
+    {id: 'TI', title: 'Health Mastery<hr>', desc:'Increase TANK fighters HP by 2000<hr>', costs:'670000 Gold & 36 Points'},
+    {id: 'TIM', title: 'Restriction of Defense Offense<hr>', desc:'Reduces TANK fighters damage taken from DPS by 10%<hr>', costs:'670000 Gold & 46 Points'},
+    {id: 'TIU', title: 'Restriction of Offense Offense<hr>', desc:'Increase TANK fighters damage dealt against DPS by 10%<hr>', costs:'670000 Gold & 56 Points'},
 
-    {id: 'FT', title: 'Fighting Arts Tree', desc:'Unlock: Player Lv 50 & 150 points in previous tree', costs:''}
+    {id: 'FT', title: 'Fighting Arts Tree<hr>', desc:'Unlock: Player Lv 50 & 150 points in previous tree', costs:''}
 ];
 
 let mkil = [
-    {id: 'GEI', title: 'General Enhancement III', desc:'Increase all fighters HP by 1600 & ATK by 200', costs:'2340000 Gold & 46 Points'},
+    {id: 'GEI', title: 'General Enhancement III<hr>', desc:'Increase all fighters HP by 1600 & ATK by 200<hr>', costs:'2340000 Gold & 46 Points'},
 
-    {id: 'DF', title: 'Crit Enhancement I', desc:'Increases DPS fighters Crit by 10%', costs:'2340000 Gold & 56 Points'},
-    {id: 'DFM', title: 'Crit Enhancement II', desc:'Increases DPS fighters Crit Damage by 30%', costs:'2340000 Gold & 66 Points'},
-    {id: 'DFU', title: 'Bloodlust', desc:'Enable DPS fighters to drain HP by 10%', costs:'2340000 Gold & 80 Points'},
+    {id: 'DF', title: 'Crit Enhancement I<hr>', desc:'Increases DPS fighters Crit by 10%<hr>', costs:'2340000 Gold & 56 Points'},
+    {id: 'DFM', title: 'Crit Enhancement II<hr>', desc:'Increases DPS fighters Crit Damage by 30%<hr>', costs:'2340000 Gold & 66 Points'},
+    {id: 'DFU', title: 'Bloodlust<hr>', desc:'Enable DPS fighters to drain HP by 10%<hr>', costs:'2340000 Gold & 80 Points'},
 
-    {id: 'OF', title: 'OF', desc:'increase rear fighters atk 200', costs:'2340000 Gold & 56 Points'},
-    {id: 'OFM', title: 'OFM', desc:'increase rear fighters atk 20%', costs:'2340000 Gold & 66 Points'},
-    {id: 'OFU', title: 'OFU', desc:'increase rear fighters damage 10%', costs:'2340000 Gold & 80 Points'},
+    {id: 'OF', title: 'DMG Enhancement<hr>', desc:'Increase GANK fighters damage by 3%<hr>', costs:'2340000 Gold & 56 Points'},
+    {id: 'OFM', title: 'Res Enhancement I<hr>', desc:'Increase GANK fighters Res Rate by 3%<hr>', costs:'2340000 Gold & 66 Points'},
+    {id: 'OFU', title: 'Slaughter<hr>', desc:'Increases Damage GANK fighters Ultimate Skills by 20%<hr>', costs:'2340000 Gold & 80 Points'},
 
-    {id: 'TI', title: 'OF', desc:'increase rear fighters atk 200', costs:'2340000 Gold & 56 Points'},
-    {id: 'TIM', title: 'OFM', desc:'increase rear fighters atk 20%', costs:'2340000 Gold & 66 Points'},
-    {id: 'TIU', title: 'OFU', desc:'increase rear fighters damage 10%', costs:'2340000 Gold & 80 Points'},
+    {id: 'TI', title: 'Block Enhancement I<hr>', desc:'Increase TANK fighters Block by 12%<hr>', costs:'2340000 Gold & 56 Points'},
+    {id: 'TIM', title: 'Res Enhancement II<hr>', desc:'Increase TANK fighters Res Rate by 3,5%<hr>', costs:'2340000 Gold & 66 Points'},
+    {id: 'TIU', title: 'Revenge<hr>', desc:'Enable TANK fighters to reflect damage by 12%<hr>', costs:'2340000 Gold & 80 Points'},
 
-    {id: 'FT', title: 'Master Skill Tree', desc:'Unlock: Player Lv 70 & 280 points in previous tree', costs:''}
+    {id: 'FT', title: 'Master Skill Tree<hr>', desc:'Unlock: Player Lv 70 & 280 points in previous tree', costs:''}
 ];
 
 let pvp = [
-    {id: 'PVP', title: 'PVP Tree', desc:'Unlock: Player Lv 80 & 520 points in previous tree', costs:''},
+    {id: 'PVP', title: 'PVP Tree<hr>', desc:'Unlock: Player Lv 80 & 520 points in previous tree', costs:''},
 
     {id: 'I', title: 'Arena Tree', desc:'', costs:''},
     {id: 'II', title: 'GEI', desc:'increase hp 560 & atk 70', costs:'415000 Gold & 36 Points'},
